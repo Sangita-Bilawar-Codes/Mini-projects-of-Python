66eafad830093d42d561003b26e2a3fafcb33aef
Sangita <sangitabilawar5@gmail.com>
2026-01-05 11:10:06 -0800

    first commit

---
+++ Basic python Tutorial.py/Operators.py
@@ -0,0 +1,48 @@
+#Arithmetic operators
+a = 5
+b = 10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+#Relational operators(Always gives True or False value)
+a = 10
+b = 8
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+#Logical operators(and, or, not)
+a = 2
+b = 4
+print(a>b or b>a)
+print(a>b and b>a)
+print(not(a>b))
+
+#Assignment operator
+num1 = 2
+num2 = 4
+num3 = 6
+num4 = 8
+num1 += 3 # num1 = num1+3 this is same to num1 += 3
+print(num1)
+num2 -= 4
+print(num2)
+num3 = num3*2 #num3 *= 2
+print(num3)
+num4 /= 4
+print(num4)
+num2 %= 3
+print(num2)
+num3 *= 1
+print(num3)
+
+
+
+

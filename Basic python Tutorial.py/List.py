d8ddd0904954e0fc4d45d906c72d181d8c9156b8
Sangita <sangitabilawar5@gmail.com>
2026-01-08 08:04:10 -0800

    first commit

---
+++ Basic python Tutorial.py/List.py
@@ -0,0 +1,25 @@
+list = [2,"Ahmed",5.2,5]
+print(list)
+print(type(list))
+print(len(list))
+
+# Indexing
+list = [2,"Ahmed",5.2,5]
+print(list[3])
+print(list[1]) == "Shahid"
+print(list)
+
+# Slicing
+list = [2,3,5.2,5]
+print(list[1:2])
+print(list[:3])
+print(list[0:])
+print(list[::])
+print(list[-4:-1])
+print(list[-2:-1])
+
+
+
+
+
+

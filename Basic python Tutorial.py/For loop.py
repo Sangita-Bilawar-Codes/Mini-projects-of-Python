64a526a92eff37694884658e8547420ec35ecd85
Sangita <sangitabilawar5@gmail.com>
2026-01-28 09:18:34 -0800

    first commit

---
+++ Basic python Tutorial.py/For loop.py
@@ -0,0 +1,16 @@
+# Write aprogram using for loop.
+Alpha = ("A" "B""C" "D")
+for i in Alpha:
+    print(Alpha)
+    
+
+# Write a program using for loop.
+num = (1,2,3,4)
+for i in num:
+    print(i)
+    
+    
+# Print the element of the following using for loop.
+nums = [1,4,9,16,25,36,49,64,81,100]
+for elements in nums:
+    print(elements)
---
+++ Miniproject.py/Miniprojests of Loop in python.py
@@ -0,0 +1,47 @@
+# Print the multiplication table of number of n.
+i = 1
+n = int(input("Enter your number:"))
+while i<=10:
+    print(n*i)
+    i+=1
+
+# Print the element of the following list using while loop.
+num = [1,4,9,16,25,36,49,64,81,100]
+idx = 0
+while idx<(len(num)):
+    print(idx)
+    print(num(idx))
+    idx+=1
+    
+    # Search for a number x in this tupule using loop.
+    num = [1,4,9,16,25,36,49,64,81,100]
+    idx = 0
+    x = 36
+    while idx<(len(num)):
+        if(x==36):
+            print("Found at idx",idx)
+        else:
+            print("Finding")
+            idx+=1
+ 
+ # Print/ search for a number x in the tupule use for loop.
+    nums = (1,4,9,16,25,36,49,64,81,100)
+    idx = 0
+     x = 81
+    for idx in nums:
+     if(idx==81):
+         print("Found at idx",idx)
+         i+=1
+         
+         
+# Write a program to find the sum of 1st natural numbers.
+    n = 1,2,3,4,5
+    i = 1
+    sum = 0
+    for i in range(1,n+1):
+        sum+=1
+        print("totalSum",sum)
+    
+    
+    
+

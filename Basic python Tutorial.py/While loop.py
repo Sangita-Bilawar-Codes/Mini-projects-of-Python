59a6c683ad269272383fcd6a506cc4b604063f3d
Sangita <sangitabilawar5@gmail.com>
2026-01-28 07:53:28 -0800

    first commit

---
+++ Basic python Tutorial.py/While loop.py
@@ -0,0 +1,24 @@
+# Write a program of while loop.
+i = 1                                                    #initialization
+while i<=9:                                               #condition
+    print(i)
+    i+=1                                                  #increment
+
+
+# Print numbers from 1 to 5.
+i = 1
+while i<5:
+    print(i)
+    i+=1
+
+# Print numbers from 5 to 1.
+i = 5
+while i>=5:
+    print(i)
+    i-=1
+    
+# Print numbers from 1 to 100.
+i = 1 
+while i<=100:
+    print(i)
+    i+=1
